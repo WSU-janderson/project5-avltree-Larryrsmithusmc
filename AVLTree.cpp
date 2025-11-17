73060423a90cec8be0a6ebfd5a3b9b1ca4e2c92d
Larryrsmithusmc <larryrsmithusmc@gmail.com>
2025-11-17 13:22:06 -0500

    Added everything for the AVLTree class and worked on insert so far.

--- AVLTree.cpp
+++ AVLTree.cpp
@@ -1,6 +1,84 @@
 #include "AVLTree.h"
-
 #include <string>
+
+
+
+bool AVLTree::insert(const std::string& key, size_t value) { // insert key-value pair into AVL tree
+    if (contains(key)) { // This checks if the key already exists and returns false if it does
+        return false;
+    }
+
+    AVLNode* newNode = new AVLNode(key, value); // create new node with key and value
+
+    if (!root) { // if tree is empty
+        root = newNode; // new node becomes root
+        return true; // insertion successful
+    }
+
+    AVLNode* current = root; // pointer starting at the root
+    AVLNode* parent = nullptr; // pointer to keep track of the parent node
+
+    while (current) {
+        parent = current; // update parent to current before moving down the tree
+        if (key < current->key) { // of key is less than current key, go left
+            current = current->left; // move left, update current then continue
+        } else { // else go right
+            current = current->right; // move right, update current then continue
+        }
+    }
+    // Now current is null and parent is the node to attach the new node to at this point
+    newNode->parent = parent; // newNode's parent is the found parent
+    if (key < parent->key) { // if key is less than parent's key
+        parent->left = newNode; // attach new node as left child
+    } else {
+        parent->right = newNode; // attach new node as right child
+    }
+    AVLNode* needsBalanced = parent; // start balancing from the parent of the new node
+    while (needsBalanced) { // will balance as long as needsBalanced is not null when null ends
+        balanceNode(needsBalanced); // balance the current node
+        needsBalanced = needsBalanced->parent; // Move to next parent for balancing if no parent null
+    }
+    return true; // insertion successful
+}
+
+bool AVLTree::remove(const std::string& key) {
+
+}
+bool AVLTree::contains(const std::string& key) const {
+
+}
+std::optional<size_t> AVLTree::get(const std::string& key) const {
+
+}
+size_t& AVLTree::operator[](const std::string& key) {
+
+}
+vector<std::string> AVLTree::findRange(const std::string& lowKey, const std::string& highKey) {
+
+}
+std::vector<std::string> AVLTree::keys() const {
+
+}
+
+size_t AVLTree::size() const {
+
+}
+size_t AVLTree::getHeight() const {
+    if (root != nullptr) {
+        return root->height;
+    }
+    return 0;
+}
+AVLTree::AVLTree(const AVLTree& other) {
+
+}
+void AVLTree::operator=(const AVLTree& other) {
+
+}
+AVLTree::~AVLTree() {
+
+}
+
 
 size_t AVLTree::AVLNode::numChildren() const {
     return 0;
@@ -63,4 +141,5 @@ bool AVLTree::remove(AVLNode *&current, KeyType key) {
 }
 
 void AVLTree::balanceNode(AVLNode *&node) {
+
 }
--- AVLTree.h
+++ AVLTree.h
@@ -4,7 +4,9 @@
 
 #ifndef AVLTREE_H
 #define AVLTREE_H
+#include <optional>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -12,6 +14,23 @@ class AVLTree {
 public:
     using KeyType = std::string;
     using ValueType = size_t;
+
+    bool insert(const std::string& key, size_t value);
+    bool remove(const std::string& key);
+    bool contains(const std::string& key) const;
+    std::optional<size_t> get(const std::string& key) const;
+    size_t& operator[](const std::string& key);
+    vector<std::string> findRange(const std::string& lowKey, const std::string& highKey);
+    std::vector<std::string> keys() const;
+    size_t size() const;
+    size_t getHeight() const;
+    AVLTree(const AVLTree& other);
+    AVLTree();
+    ~AVLTree();
+    void operator=(const AVLTree& other);
+
+
+    friend std::ostream& operator<<(ostream& os, const AVLTree & avlTree);
 
 protected:
     class AVLNode {
@@ -22,6 +41,8 @@ protected:
 
         AVLNode* left;
         AVLNode* right;
+        AVLNode* parent;
+
 
         // 0, 1 or 2
         size_t numChildren() const;
@@ -44,7 +65,7 @@ public:
     /* Helper methods for remove */
     // this overloaded remove will do the recursion to remove the node
     bool remove(AVLNode*& current, KeyType key);
-    // removeNode contains the logic for actually removing a node based on the numebr of children
+    // removeNode contains the logic for actually removing a node based on the number of children
     bool removeNode(AVLNode*& current);
     // You will implement this, but it is needed for removeNode()
     void balanceNode(AVLNode*& node);
